415acbc1c9e730d27addf0d31db543c8d44f5494
Mike Elson <mike.elson@gmail.com>
2013-10-25 02:14:07 +0200

    dist

--- setup.py
+++ setup.py
@@ -6,20 +6,21 @@ from themelog import __version__
 
 setup(name='themelog',
 	  version=__version__,
+	  # py_modules=['themelog'],
 	  description='256-colour log messages',
+	  long_description=open('README.rst').read(),
 	  author='Mike Elson',
 	  author_email='mike.elson@gmail.com',
-	  url='github.com/mjem/themelog',
-	  packages=['themelog'],
+	  url='http://github.com/mjem/themelog',
+	  packages=['themelog', 'themelog.themes'],
+	  data_files=[('.', ['README.rst'])],
+	  license='Apache-2',
+	  requires=['fabulous'],
+	  # install_requires=['fabulous'],
 	  classifiers=[
-		'Development Status :: 4 - Beta',
-		'Environment :: X11 Applications :: GTK',
-		'Intended Audience :: End Users/Desktop',
+		'Environment :: Console',
 		'Intended Audience :: Developers',
-		'License :: OSI Approved :: GNU General Public License (GPL)',
+		'License :: OSI Approved :: Apache Software License',
 		'Operating System :: POSIX :: Linux',
-		'Programming Language :: Python',
-		'Topic :: Desktop Environment',
-		'Topic :: Text Processing :: Fonts'
       ]
 	  )
